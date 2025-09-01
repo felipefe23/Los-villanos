0e24d919df50888dfbc924793848191d5c207e52
Diego Rojas <dirojas24@alumnos.utalca.cl>
2025-08-31 23:19:28 -0400

    Testing HTML con la integración de API.
    Herramientas: 
    Leaflet (Uso de API para Mapeo)
    Flask (Uso para la conexión de los datos)
    Carpetas y docs:
    /Testing/helpers.py ---> Este doc. es de uso opcional ya que el Menú se agregó en el Frontend.
    
    Esto es solo un TEST.

---
+++ Testing/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, jsonify
+from persistencia.manejoArchivo import leer_propiedades, guardar_propiedades
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/api/propiedades', methods=['GET'])
+def get_propiedades():
+    propiedades = leer_propiedades()
+    return jsonify(propiedades)
+
+@app.route('/api/propiedades', methods=['POST'])
+def add_propiedad():
+    data = request.json
+    direccion = data.get('direccion')
+    precio = data.get('precio')
+
+    try:
+        precio = int(precio)
+    except (ValueError, TypeError):
+        return jsonify({"error": "El precio debe ser un nmero."}), 400
+
+    propiedades = leer_propiedades()
+    nueva = {
+        "id": len(propiedades) + 1,
+        "direccion": direccion,
+        "precio": precio
+    }
+    propiedades.append(nueva)
+    guardar_propiedades(propiedades)
+    return jsonify({"message": "Todo salio bien wn xd", "propiedades": propiedades})
+
+if __name__ == '__main__':
+    app.run(debug=True)
